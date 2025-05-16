9254755bae88e3d778ec52c113e98426e27a39af
Victor Chicu <victor@aroundwire.com>
2025-05-16 15:51:55 +0300

    Grok template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cv-app';
+  link: string = '';
+
+  generate(): void {
+    if (this.link.trim()) {
+
+    }
+  }
 }

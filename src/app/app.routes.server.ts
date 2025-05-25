a6cf7d99bf2534d61e02476a086c1c2956d2f7a4
Victor Chicu <victor.chicu@hotmail.com>
2025-05-25 23:28:49 +0300

    Render resume-draft/:profileId on server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'resume-draft/:profileId',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }

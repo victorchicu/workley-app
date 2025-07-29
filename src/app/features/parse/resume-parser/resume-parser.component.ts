621d0bf53fa493a735fe41dadcaa86dd60d9a918
Victor Chicu <victor.chicu@hotmail.com>
2025-07-29 22:49:17 +0300

    Add resume parse component

---
+++ src/app/features/parse/resume-parser/resume-parser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-parser',
+  imports: [],
+  templateUrl: './resume-parser.component.html',
+  styleUrl: './resume-parser.component.css'
+})
+export class ResumeParserComponent {
+
+}

692d06cfa1cb3f0fa02e57223e939c29cbe66f64
Victor Chicu <victor.chicu@hotmail.com>
2025-08-04 20:51:58 +0300

    Add 2 screen (agent chat)

---
+++ src/app/features/agent-chat/agent-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './agent-chat.component.html',
+  styleUrl: './agent-chat.component.css'
+})
+export class AgentChatComponent {
+
+}

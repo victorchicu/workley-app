079110dd531ed4c4bc0d77db4b7ea5a1ebf1f0d1
Victor Chicu <victor.chicu@hotmail.com>
2025-08-04 21:07:08 +0300

    Add 3 screen (resume editor)

---
+++ src/app/features/resume-editor/resume-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-editing',
+  imports: [],
+  templateUrl: './resume-editor.component.html',
+  styleUrl: './resume-editor.component.css'
+})
+export class ResumeEditorComponent {
+
+}

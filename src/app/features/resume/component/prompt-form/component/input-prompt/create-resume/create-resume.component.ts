e5aa9e68f7ca75766ff1d0023affdb2db2da620d
Victor Chicu <victor@aroundwire.com>
2025-08-05 10:31:56 +0300

    Add shared loader to create resume button

--- src/app/features/resume/component/prompt-form/component/input-prompt/create-resume/create-resume.component.ts
+++ src/app/features/resume/component/prompt-form/component/input-prompt/create-resume/create-resume.component.ts
@@ -1,14 +1,17 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {PromptControl, PromptFormGroup} from "../input-prompt.component";
 import {FormGroup} from '@angular/forms';
-import {NgClass, NgIf} from '@angular/common';
+import {AsyncPipe, NgClass, NgIf} from '@angular/common';
+import {LoaderService} from '../../../../../../../core/service/loader.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-create-resume',
   standalone: true,
   imports: [
     NgClass,
-    NgIf
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './create-resume.component.html',
   styleUrl: './create-resume.component.css'
@@ -16,14 +19,18 @@ import {NgClass, NgIf} from '@angular/common';
 export class CreateResumeComponent {
 
   @Input() activated: boolean = false;
-  @Input() isLoading: boolean = false;
-
   @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
+
+  loading$: Observable<boolean>;
+
+  constructor(private readonly loader: LoaderService) {
+    this.loading$ = loader.loading$;
+  }
 
   handleClick() {
     console.log("Handle resume creation click")
     if (this.activated) {
-      if (!this.isLoading) {
+      if (!this.loader.loading) {
         this.onClick.emit()
       }
     }

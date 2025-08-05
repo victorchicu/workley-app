71d18f6e9b6775c7ad6de2418fea22e3cf62cc61
Victor Chicu <victor@aroundwire.com>
2025-08-05 10:28:20 +0300

    Add something went wrong component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {PromptFormComponent} from './features/resume/component/prompt-form/prompt-form.component';
 import {AgentChatComponent} from './features/resume/component/agent-chat/agent-chat.component';
+import {
+  SomethingWentWrongComponent
+} from './shared/component/main/something-went-wrong/something-went-wrong.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +15,11 @@ export const routes: Routes = [
     path: 'resume/:resumeId',
     component: AgentChatComponent,
     title: 'Free Resume Builder | Resume AI',
+  },
+  {
+    path: 'error',
+    component: SomethingWentWrongComponent,
+    title: 'Free Resume Builder | Something Went Wrong',
   },
   {
     path: '**',

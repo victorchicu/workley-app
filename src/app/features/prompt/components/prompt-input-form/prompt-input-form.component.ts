608edd636b4b78a1a0aa78e22ca36cb5ccf55449
Victor Chicu <victor.chicu@hotmail.com>
2025-09-27 18:00:18 +0300

    Adjut prompt input placeholder

--- src/app/features/prompt/components/prompt-input-form/prompt-input-form.component.ts
+++ src/app/features/prompt/components/prompt-input-form/prompt-input-form.component.ts
@@ -27,7 +27,7 @@ import {PromptControl} from '../../prompt.component';
 export class PromptInputFormComponent {
   readonly form = input.required<FormGroup<PromptControl>>();
   readonly error = input<string | null>(null);
-  readonly placeholder = input("How can I help you today?");
+  readonly placeholder = input("Tell me what you\'re looking for?");
   readonly isSubmitting = input<boolean>(false);
   readonly isDeactivated = input(false);
   readonly isLineWrapped = input<boolean>(false);
